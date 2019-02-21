e7fcb70c7a65ba8cc76216ae56ab67d7ad8dc003
Julius Turunen <julius.turunen@aalto.fi>
2019-02-21 02:01:03 +0200

    refactor: reassigned parsing json

--- src/main/scala/com/lunchlist/io/Configurations.scala
+++ src/main/scala/com/lunchlist/io/Configurations.scala
@@ -1,31 +1,16 @@
 package com.lunchlist.io
 
-import collection.mutable.ListBuffer
 import io.Source
 
-import play.api.libs.json._
-
-import com.lunchlist.restaurant._
+import com.lunchlist.json.JsonParser.restaurantsToList
 
 object Configurations {
-  val configurationsFilePath = "./data/configurations.json"
-  val config = Json.parse(Source.fromFile(configurationsFilePath).getLines.reduceLeft(_ + "\n" + _))
-  
-  def loadRestaurants(): List[Restaurant] = {
-    val restaurants: ListBuffer[Restaurant] = new ListBuffer()
-    val restaurantsJSON = (config \ "restaurants").as[List[Map[String, String]]]
-    for(restaurant <- restaurantsJSON){
-      val rType = restaurant("type")
-      val name = restaurant("name")
-      val id = restaurant("id")
-      var r: Restaurant = null
-      rType match {
-        case "fazer" => { r = new FazerRestaurant(name, id) }
-        case "sodexo" => { r = new SodexoRestaurant(name, id) }
-        case unknownType => println(s"Unknown restaurant type '$rType'")
-      }
-      restaurants += r
-    }
-    return restaurants.toList
+  def readFromFile(path: String): String = {
+    val source = Source.fromFile(path)
+    val raw = source.getLines.reduceLeft(_ + "\n" + _)
+    return raw
   }
+  private val configFilePath = "./data/configurations.json"
+  private val configRaw = readFromFile(configFilePath)
+  def loadRestaurants() = restaurantsToList(configRaw)
 }
