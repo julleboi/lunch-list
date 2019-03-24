ba118f104e5a899a23fc97742f0a62dab5d74357
Julius Turunen <julius.turunen@aalto.fi>
2019-03-24 16:52:13 +0200

    removed unused import

--- src/main/scala/com/lunchlist/restaurant/SodexoRestaurant.scala
+++ src/main/scala/com/lunchlist/restaurant/SodexoRestaurant.scala
@@ -1,7 +1,5 @@
 package com.lunchlist.restaurant
 
-import collection.mutable.ListBuffer
-
 import com.lunchlist.util.DateTools._
 
 class SodexoRestaurant(name: String, id: String) extends Restaurant(name, id) {
