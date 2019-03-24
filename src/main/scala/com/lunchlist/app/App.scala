6d95f82badbb4cf89d53dd9ee480590f62ec5b47
Julius Turunen <julius.turunen@aalto.fi>
2019-03-24 14:22:01 +0200

    renaming

--- src/main/scala/com/lunchlist/app/App.scala
+++ src/main/scala/com/lunchlist/app/App.scala
@@ -21,12 +21,12 @@ object App {
   val loadMenus: Command = _ => loadAllMenus
 
   def printAvailableActions() = {
-    println("Abvailable actions:")
+    println("Abvailable commands:")
     println(inputToCommand.keys.map("-"+_).reduceLeft(_ + ", " + _))
   }
   val printCommands: Command = _ => printAvailableActions
 
-  val printUsage: Command = _ => println("Usage: '-noOptions -withOptions=<options for this command> ...'")
+  val printUsage: Command = _ => println("Usage: '-noOptions -withOptions=<options for this command> ...'\nCommands are executed in order.")
 
   def terminate() = {
     print("Invalid command, ")
