5ea297052474020c1610d8382a94655e6ccf3590
Julius Turunen <julius.turunen@aalto.fi>
2019-04-17 12:05:53 +0300

    New tests for datetools

--- src/test/scala/DateToolsTest.scala
+++ src/test/scala/DateToolsTest.scala
@@ -1,12 +1,20 @@
-import com.lunchlist.util.DateTools.{getDay}
+import com.lunchlist.util.DateTools.{ getDay, getDatesThisWeek, stringToDate, getDate }
 
 import org.scalatest._
 
 class DateToolsTest extends FlatSpec {
   
-  "DateTools.getDay(date: java.util.Date)" should "return 'Thursday' with Date(01/01/1970)" in {
+  "getDay(date)" should "return 'Thursday' with Date(01/01/1970)" in {
     val dateObject = new java.util.Date(0L)
     assert(getDay(dateObject) == "Thursday")
+  }
+
+  "getDatesThisWeek()" should "return List[String] of length 7" in {
+    assert(getDatesThisWeek().length == 7)
+  }
+
+  """stringToDate("1970-01-01")""" should "return a correct Date object" in {
+    assert(getDate("yyyy/MM/dd", stringToDate("1970-01-01")) == getDate("yyyy/MM/dd", new java.util.Date(0)))
   }
 
 }
