2c9a2d2adfcc2733e1f4348b6b7fcddb1d559c40
Julius Turunen <julius.turunen@aalto.fi>
2019-04-17 12:06:21 +0300

    Add new tests for Menus

---
+++ src/test/scala/MenuTest.scala
@@ -0,0 +1,43 @@
+import com.lunchlist.restaurant._
+
+import org.scalatest._
+
+class MenuTest extends FlatSpec {
+
+  val steakLunch = Food("Steak lunch", List[Component](Component("Rib-Eye steak (G, M, L)"), Component("Potatoes (G, M, L)")))
+  val veganLunch = Food("Vegan lunch", List[Component](Component("Vegan pizza (V, M, L)")))
+  val dessert = Food("Dessert", List[Component](Component("Chocolate mousse (G)")))
+  
+  val exFoods = List[Food](
+    steakLunch,
+    veganLunch,
+    dessert
+  )
+
+  // copies exFoods list without changing anything
+  def exampleFoods = exFoods.map(f => f.copy(name = f.name))
+
+  "Component method containsFilteredProps(filteredProps)" should "work correctly" in {
+    // Steak lunch is milkfree and glutenfree
+    assert(exampleFoods(0).containsFilteredProps(List[Property](MilkFree)))
+
+    // Vegan lunch is vegan
+    assert(exampleFoods(1).containsFilteredProps(List[Property](Vegan)))
+
+    // Dessert is NOT vegan
+    assert(!exampleFoods(2).containsFilteredProps(List[Property](Vegan)))
+  }
+
+  "Filtering menu for various components" should "return correctly filtered menus" in {
+    val vegan = new Menu("Monday", exampleFoods)
+    vegan.filterForProperties(List[Property](Vegan))
+    // Foods list should now only contain vegan lunch
+    assert(vegan.getFoods().forall(_.name == "Vegan lunch"))
+
+    val glutenfree = new Menu("Tuesday", exampleFoods)
+    glutenfree.filterForProperties(List[Property](GlutenFree))
+    // foods list should now only contain Steak lunch or Dessert
+    assert(glutenfree.getFoods().forall(f => f.name == "Steak lunch" || f.name == "Dessert"))
+  }
+
+}
